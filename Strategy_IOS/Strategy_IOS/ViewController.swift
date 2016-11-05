4ea6803366bd3725e3776a1ff46b94f845f9fe3f
Leo428 <huzheyuan428@gmail.com>
2016-11-05 14:22:24 -0700

    The first commit!

---
+++ Strategy_IOS/Strategy_IOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Strategy_IOS
+//
+//  Created by 胡哲源 on 11/5/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myB: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
